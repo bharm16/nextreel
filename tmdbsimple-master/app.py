6c959ce277f9dbb5964c49f2c6ea708019b72f46
Bryce Harmon <bph40@txstate.edu>
2023-08-25 20:10:37 -0500

    added userLogin.html page that serves as a login page

--- tmdbsimple-master/app.py
+++ tmdbsimple-master/app.py
@@ -9,8 +9,8 @@ from flask_login import LoginManager, login_user, login_required, logout_user, c
 
 app = Flask(__name__)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 
 print("Current working directory:", os.getcwd())
