7476ec6733305fb41d301725adcc0ed4eeff8e93
Bryce Harmon <bph40@txstate.edu>
2023-09-16 03:28:10 -0500

    updating title.basics slowly

--- nextreel/scripts/logMovieToAccount.py
+++ nextreel/scripts/logMovieToAccount.py
@@ -61,6 +61,41 @@ def update_title_basics_if_empty(tconst, plot, poster_url, db_config):
         """
         execute_query(db_config, update_query, (plot, poster_url, tconst), fetch='none')
 
+
+def update_missing_title_info(db_config):
+    # Step 1: Retrieve up to 100 tconst values where either plot or poster_url is empty
+    query = """
+    SELECT tconst 
+    FROM `title.basics`
+    WHERE plot IS NULL
+    AND poster_url IS NULL
+    AND titleType = 'movie'
+    LIMIT 100;
+    """
+    result = execute_query(db_config, query, fetch='all')
+
+    if not result:
+        print("No records need updating.")
+        return
+
+    # Step 2 and 3: Loop through the tconst values, fetch from IMDb, and update
+    for row in result:
+        tconst = row['tconst']
+
+        print(f"Fetching information for {tconst}...")
+
+        # Fetch from IMDb
+        movie = fetch_movie_info_from_imdb(tconst)
+        plot = movie.get('plot outline')  # Assuming 'plot outline' is the key for plot
+        poster_url = movie.get('cover url')  # Assuming 'cover url' is the key for poster URL
+
+        # Update the title.basics table
+        update_title_basics_if_empty(tconst, plot, poster_url, db_config)
+
+
+# Now you can call this function to update up to 100 missing information
+update_missing_title_info(db_config)
+
 # if __name__ == "__main__":
 # db_config = {
 #     'host': 'localhost',
