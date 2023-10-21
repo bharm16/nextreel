2d1716f72c8a1d62d095d8a969a140caf828c463
Bryce Harmon <bph40@txstate.edu>
2023-10-21 16:05:09 -0500

    added ability to get cast images from tmdb. still need to add to html

--- nextreel/scripts/tmdb_data.py
+++ nextreel/scripts/tmdb_data.py
@@ -1,50 +1,71 @@
 import tmdbsimple as tmdb
 
+# Initialize API Key
 tmdb.API_KEY = '1ce9398920594a5521f0d53e9b33c52f'
 
-movie = tmdb.Movies(603)
-response = movie.info()
 
-print(response)
-
-tconst = movie.external_ids()
-
-print(tconst)
-
-
-
+# Function to fetch TMDb ID using IMDb tconst
 def get_tmdb_id_by_tconst(tconst):
-    """Fetch TMDb ID using IMDb tconst."""
     find = tmdb.Find(tconst)
     response = find.info(external_source='imdb_id')
     tmdb_id = response['movie_results'][0]['id'] if response['movie_results'] else None
     return tmdb_id
 
 
+# Function to fetch movie information by TMDb ID
 def get_movie_info_by_tmdb_id(tmdb_id):
-    """Fetch movie information by TMDb ID."""
     movie = tmdb.Movies(tmdb_id)
     response = movie.info()
     return response
 
 
+# Function to fetch cast information by TMDb ID
+def get_cast_info_by_tmdb_id(tmdb_id):
+    movie = tmdb.Movies(tmdb_id)
+    response = movie.credits()
+    return response.get('cast', [])
+
+
+# Function to get full image URL
+def get_full_image_url(profile_path, size='w185'):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}{profile_path}"
+
+
+# Class for managing TMDb movie information
 class TmdbMovieInfo:
     def __init__(self, api_key):
         self.api_key = api_key
         tmdb.API_KEY = self.api_key
 
 
-# Main function to run the program
+# Main function
 def main(api_key, tconst):
+    # Initialize TMDb info
     tmdb_info = TmdbMovieInfo(api_key)
 
-    # Fetch TMDb ID using IMDb tconst
+    # Fetch TMDb ID
     tmdb_id = get_tmdb_id_by_tconst(tconst)
 
     if tmdb_id:
-        # Fetch and print movie information by TMDb ID
+        # Fetch and display movie information
         movie_info = get_movie_info_by_tmdb_id(tmdb_id)
         print("Movie Information from TMDb:", movie_info)
+
+        # Fetch and display cast information
+        cast_info = get_cast_info_by_tmdb_id(tmdb_id)
+        print("Cast Information:")
+        for cast_member in cast_info:
+            print(f"{cast_member['name']} as {cast_member['character']}")
+
+            # Fetch and display cast image URL
+            profile_path = cast_member.get('profile_path')
+            if profile_path:
+                image_url = get_full_image_url(profile_path)
+                print(f"Image URL: {image_url}")
+            else:
+                print("Image not available")
+
     else:
         print("TMDb ID not found.")
 
